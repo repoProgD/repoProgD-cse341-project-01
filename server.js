fce5e715a6c2cf868696b5faf70f3d864b6adeaa
unknown <diegoprogra28@gmail.com>
2026-09-05 18:29:06 -0300

    assigmentW01 done

---
+++ server.js
@@ -0,0 +1,29 @@
+// Import Express to create and configure the web server
+const express = require('express');
+
+// Workaround for DNS resolution issues (Node is using 127.0.0.1 instead of a public DNS server)
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+const mongodb = require('./data/database');
+// Create a new Express application instance
+const app = express();
+
+
+// server port that will listen for incoming requests from the frontend
+const port = process.env.PORT || 3000;
+
+app.use('/', require('./routes'));
+
+mongodb.initDB((err) => {
+    if (err){
+        console.log(err);
+    }
+    else {
+        // Start "listening" for requests
+        app.listen(port, () => {
+            console.log(`DataBase is listening and Node is running on port ${port}`);
+        });
+    }
+}) 
+
